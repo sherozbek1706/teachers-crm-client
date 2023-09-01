46f181a0d2033155bb7ccde550a2f58723b5d1f3
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 22:43:08 +0500

    api va tokenni berib yubordim.

--- src/shared/services/axios.js
+++ src/shared/services/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { api } from "../const/url.js";
 const ACCESS_TOKEN = localStorage.getItem("token") || "";
 export const axiosInstance = axios.create({
+  baseURL: api,
   headers: {
     "Content-type": "application/json",
+    Authorization: ACCESS_TOKEN,
   },
 });
