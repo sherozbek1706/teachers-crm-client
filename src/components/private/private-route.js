6db6c01c81e365dd98bc47b878965d705790f682
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 22:52:03 +0500

    useEffect yordamida navigate qilib yubordim. Login pagega

--- src/components/private/private-route.js
+++ src/components/private/private-route.js
@@ -3,6 +3,12 @@ import { useNavigate } from "react-router-dom";
 export const Protected = ({ isLoggedIn, children }) => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login", { replace: true });
+    }
+  }, [isLoggedIn, navigate]);
+
   if (!isLoggedIn) {
     return null;
   }
