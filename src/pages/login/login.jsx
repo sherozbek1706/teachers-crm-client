c1d4906559bc3c46c07a639d23d112a95aa34182
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 23:20:10 +0500

    navigate'ni useNavigate'dan oldim.

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,7 @@
 import "./login.css";
+import { useNavigate } from "react-router-dom";
 export const Login = () => {
+  const navigate = useNavigate();
   return (
     <div className="Login">
     </div>
