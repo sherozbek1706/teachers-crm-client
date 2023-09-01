2f4b100290e16263122fcb71f79ae1ed01c2161e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 22:42:54 +0500

    ACCESS_TOKEN'ni local storagedan oldim yoki hech narsa

--- src/shared/services/axios.js
+++ src/shared/services/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { api } from "../const/url.js";
+const ACCESS_TOKEN = localStorage.getItem("token") || "";
 export const axiosInstance = axios.create({
   headers: {
     "Content-type": "application/json",
