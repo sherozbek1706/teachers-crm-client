cffb8d9b3f0aa40f7fbcea7c4961858b04b33be2
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 23:16:30 +0500

    Sitebar import qilingan.

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,9 @@
+import { Sitebar } from "../../layouts";
 import "./dashboard.css";
 export const Dashboard = () => {
   return (
     <div className="Dashboard">
+      <Sitebar />
       <h1>Dashboard</h1>
     </div>
   );
