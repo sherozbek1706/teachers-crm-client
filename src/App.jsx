5309e84f2c69acf24ca4567a8fa3d3ddecf39004
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 22:47:34 +0500

    react-router-dom yordamida yo'llarni berib yubordim.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Route, Routes as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
+import { Dashboard, Login } from "./pages";
 export const App = () => {
   const token = localStorage.getItem("token") ? true : false;
 
   return (
     <div className="App">
+      <Router>
+        <Route
+          exact
+          path="/"
+          element={
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
+      </Router>
     </div>
   );
 };
