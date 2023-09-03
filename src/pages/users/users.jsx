70895a2823610bff7c9732d985af6372cc2cb080
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-03 23:09:09 +0500

    Users__list'larni yozib berdim.

--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -1,6 +1,15 @@
 export const Users = () => {
   return (
     <div className="Users__dashboard">
+      <Sitebar />
+      <div className="Users">
+        <div className="Users__header">
+          <h1 className="Users__header__title">All Users</h1>
+        </div>
+        <div className="Users__options"></div>
+        <div className="Users__list">
+        </div>
+      </div>
     </div>
   );
 };
