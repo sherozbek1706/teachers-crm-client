888782d20e63508fb1ea24b5915a810eb796b4cf
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-03 22:45:57 +0500

    Userning profile qismi hozircha shadow qismlari qo'yib chiqidi!

--- src/layouts/profile/profile.jsx
+++ src/layouts/profile/profile.jsx
@@ -1,5 +1,32 @@
 import "./profile.css";
 export const Profile = () => {
   return (
+    <div className="Profile__Dashboard">
+      <div className="Profile">
+        <div className="Profile__header">
+          <h1 className="Profile__header__title">User Profile</h1>
+        </div>
+        <div className="Profile__infos">
+          <div className="Profile__info">
+            <p className="Profile__info__shadow">ID</p>
+          </div>
+          <div className="Profile__info">
+            <p className="Profile__info__shadow">FIRST NAME</p>
+          </div>
+          <div className="Profile__info">
+            <p className="Profile__info__shadow">LAST NAME</p>
+          </div>
+          <div className="Profile__info">
+            <p className="Profile__info__shadow">USERNAME</p>
+          </div>
+          <div className="Profile__info">
+            <p className="Profile__info__shadow">AGE</p>
+          </div>
+          <div className="Profile__info">
+            <p className="Profile__info__shadow">ROLE</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
