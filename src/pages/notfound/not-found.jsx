965c3c19a6669d9c94b5ea16d34d9c2852f52b4e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-05 09:32:49 +0500

    react router domlarni uladim.

--- src/pages/notfound/not-found.jsx
+++ src/pages/notfound/not-found.jsx
@@ -1,10 +1,13 @@
 import "./not-found.css";
+import { Link } from "react-router-dom";
 export const NotFound = () => {
   return (
     <div className="NotFound">
       <h1 className="number">404</h1>
       <h1 className="title">Not Found</h1>
+      <Link to="/profile">
         <button className="return">Profile</button>
+      </Link>
     </div>
   );
 };
