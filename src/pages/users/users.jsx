f614cb79d274fdfa690cc4b855efccfbc8e87bf9
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-05 15:42:53 +0500

    userslarni map qilib chiqdim.

--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -1,29 +1,55 @@
 import { useEffect, useState } from "react";
 import { Sitebar } from "../../layouts";
 import "./users.css";
+import { AiFillEye } from "react-icons/ai";
+import { BiEdit, BiTrash } from "react-icons/bi";
 import { axiosInstance } from "../../shared/services/axios";
 import { Loader } from "../../components";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 export const Users = () => {
   const navigate = useNavigate();
   const [usersData, setUsersData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    axiosInstance
-      .get("/users")
-      .then((res) => {
-        setUsersData([res.data.data]);
-      })
-      .catch((err) => {
-        if (err.response.data.error == "This user is not allowed this right!") {
-          navigate("/");
-        }
-      });
+    fetchData(); // Fetch data when the component mounts
   }, []);
 
-  if (!usersData.length) {
-    return <Loader />;
-  }
+  const fetchData = () => {
+    axiosInstance
+      .get("/users")
+      .then(({ data }) => {
+        setUsersData(data.data);
+        setLoading(false);
+      })
+      .catch(handleError);
+  };
 
+  const handleError = (err) => {
+    if (
+      err.response &&
+      err.response.data.error === "This user is not allowed this right!"
+    ) {
+      navigate("/");
+    }
+  };
+
+  const handleRemoveUser = (id) => {
+    axiosInstance
+      .delete(`/users/${id}`)
+      .then(() => {
+        // Remove the deleted user from the state
+        setUsersData((prevUsersData) =>
+          prevUsersData.filter((user) => user.id !== id)
+        );
+      })
+      .catch(handleError)
+      .finally(() => {
+        // Refetch data after removal
+        fetchData();
+      });
+  };
   return (
     <div className="Users__dashboard">
       <Sitebar />
@@ -33,9 +59,48 @@ export const Users = () => {
         </div>
         <div className="Users__options"></div>
         <div className="Users__list">
-          {usersData[0].map((user) => (
-            <div className="Users__row"></div>
-          ))}
+          <div className="Users__row">
+            <p className="Users__text Users__firstname">
+              <b>First Name</b>
+            </p>
+            <p className="Users__text Users__lastname">
+              <b>Last Name</b>
+            </p>
+            <p className="Users__text Users__role">
+              <b>Role</b>
+            </p>
+            <p className="Users__text Users__age">
+              <b>Age</b>
+            </p>
+            <p className="Users__text Users__username">
+              <b>Username</b>
+            </p>
+          </div>
+          {loading ? (
+            <Loader />
+          ) : (
+            usersData.map((user) => (
+              <div className="Users__row" key={user.id}>
+                <p className="Users__text Users__firstname">
+                  {user.first_name}
+                </p>
+                <p className="Users__text Users__lastname">{user.last_name}</p>
+                <p className="Users__text Users__role">{user.role}</p>
+                <p className="Users__text Users__age">{user.age}</p>
+                <p className="Users__text Users__username">@{user.username}</p>
+                <div className="Users__option">
+                  <Link to={`/profile/${user.id}`}>
+                    <AiFillEye className="Users__icons Users__view" />
+                  </Link>
+                  <BiEdit className="Users__icons Users__edit" />
+                  <BiTrash
+                    className="Users__icons Users__delete"
+                    onClick={() => handleRemoveUser(user.id)}
+                  />
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </div>
