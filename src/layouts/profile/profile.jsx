93d62dddeee55663cbeaf531ed602e5c2ac80b33
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-05 15:40:43 +0500

    userData optimizatsiyalashdi!

--- src/layouts/profile/profile.jsx
+++ src/layouts/profile/profile.jsx
@@ -6,6 +6,7 @@ import { Loader } from "../../components";
 
 export const Profile = () => {
   const [userData, setUserData] = useState([]);
+  const { id = "me" } = useParams();
   useEffect(() => {
     axiosInstance.get("users/me").then((res) => {
       setUserData([res.data.data]);
@@ -16,9 +17,12 @@ export const Profile = () => {
   if (!userData.length) {
     return <Loader />;
   }
+
+  const { _id, first_name, last_name, username, age, role } = userData;
+
   return (
     <div className="Profile__Dashboard">
-      <Sitebar data={userData[0].role} />
+      <Sitebar data={id == "me" ? role : "admin"} />
       <div className="Profile">
         <div className="Profile__header">
           <h1 className="Profile__header__title">User Profile</h1>
@@ -26,27 +30,27 @@ export const Profile = () => {
         <div className="Profile__infos">
           <div className="Profile__info">
             <p className="Profile__info__shadow">ID</p>
-            <p className="Profile__info__text">{userData[0]?._id}</p>
+            <p className="Profile__info__text">{_id}</p>
           </div>
           <div className="Profile__info">
             <p className="Profile__info__shadow">FIRST NAME</p>
-            <p className="Profile__info__text">{userData[0]?.first_name}</p>
+            <p className="Profile__info__text">{first_name}</p>
           </div>
           <div className="Profile__info">
             <p className="Profile__info__shadow">LAST NAME</p>
-            <p className="Profile__info__text">{userData[0]?.last_name}</p>
+            <p className="Profile__info__text">{last_name}</p>
           </div>
           <div className="Profile__info">
             <p className="Profile__info__shadow">USERNAME</p>
-            <p className="Profile__info__text">{userData[0]?.username}</p>
+            <p className="Profile__info__text">{username}</p>
           </div>
           <div className="Profile__info">
             <p className="Profile__info__shadow">AGE</p>
-            <p className="Profile__info__text">{userData[0]?.age}</p>
+            <p className="Profile__info__text">{age}</p>
           </div>
           <div className="Profile__info">
             <p className="Profile__info__shadow">ROLE</p>
-            <p className="Profile__info__text">{userData[0]?.role}</p>
+            <p className="Profile__info__text">{role}</p>
           </div>
         </div>
       </div>
