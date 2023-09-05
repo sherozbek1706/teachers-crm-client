09aae7f3e5e0a13085069ffa92bf09875888e277
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-05 09:32:37 +0500

    NotFound page ochib oldim va kodni yozdim.

---
+++ src/pages/notfound/not-found.jsx
@@ -0,0 +1,10 @@
+import "./not-found.css";
+export const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <h1 className="number">404</h1>
+      <h1 className="title">Not Found</h1>
+        <button className="return">Profile</button>
+    </div>
+  );
+};
