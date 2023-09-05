4f6ea8832ce153baa2317887ef6a14f9914fa9c9
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-05 09:39:17 +0500

    users.jsx ni prettier bilan tozalandi!

--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -3,12 +3,16 @@ import { Sitebar } from "../../layouts";
 import "./users.css";
 import { axiosInstance } from "../../shared/services/axios";
 import { Loader } from "../../components";
+import { useNavigate } from "react-router-dom";
 export const Users = () => {
+  const navigate = useNavigate();
   const [usersData, setUsersData] = useState([]);
   useEffect(() => {
-    axiosInstance.get("/users").then((res) => {
-      setUsersData([res.data.data]);
-    });
+    axiosInstance
+      .get("/users")
+      .then((res) => {
+        setUsersData([res.data.data]);
+      })
   }, []);
 
   if (!usersData.length) {
@@ -25,9 +29,7 @@ export const Users = () => {
         <div className="Users__options"></div>
         <div className="Users__list">
           {usersData[0].map((user) => (
-            <div className="Users__row">
-              
-            </div>
+            <div className="Users__row"></div>
           ))}
         </div>
       </div>
