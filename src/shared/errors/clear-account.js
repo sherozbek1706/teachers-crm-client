486c248dabb86fc1589fae257d8eb674f8501f96
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-07 18:29:04 +0500

    errorlarni ushlab olsih. swich orqali aniqlab olish

--- src/shared/errors/clear-account.js
+++ src/shared/errors/clear-account.js
@@ -1,5 +1,23 @@
 
 export const HandleFetchError = (err) => {
   if (err.response) {
+    const error = err.response.data.error;
+
+    switch (error) {
+      case "jwt expired":
+        jwt_expired();
+        break;
+
+      case "This user is not allowed this right!":
+        not_allow();
+        break;
+
+      case "invalid signature":
+        invalid_signature();
+        break;
+
+      default:
+        errorNot(error);
+    }
   }
 };
