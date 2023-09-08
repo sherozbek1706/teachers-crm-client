e40018766d28628f0234aa5c70b37e28ba39b482
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-08 09:32:37 +0500

    Jsx yozib oldim loaderni berib yuborishim qoldi.

--- src/pages/guides/guides.jsx
+++ src/pages/guides/guides.jsx
@@ -1,3 +1,9 @@
+import { useEffect, useState } from "react";
+import { GuideList, Sitebar } from "../../layouts";
+import "./guides.css";
+import { axiosInstance } from "../../shared/services/axios";
+import { HandleFetchError } from "../../shared/errors/clear-account";
+import { Loader } from "../../components";
 export const Guides = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -16,6 +22,12 @@ export const Guides = () => {
 
   return (
     <div className="Guides__dashboard">
+      <Sitebar />
+      <div className="Guides">
+        <div className="Guides__header">
+          <h1 className="Guides__header__title">All Guides</h1>
+        </div>
+      </div>
     </div>
   );
 };
