5627a0c4e8c0ab0f41a5278a2d214ec316ca844e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-08 09:36:21 +0500

    Guides va MyGuides barcha userlar uchun bor shuning uchun olib chiqdim tashqariga

--- src/layouts/sitebar/sitebar.jsx
+++ src/layouts/sitebar/sitebar.jsx
@@ -22,15 +22,7 @@ export const Sitebar = ({ data = localStorage.getItem("role") }) => {
             <Link to="/users" className="Sitebar__btn">
               Users
             </Link>
-            <Link to="" className="Sitebar__btn">
-              Guides
-            </Link>
           </>
-        ) : (
-          <Link to="" className="Sitebar__btn">
-            My Guides
-          </Link>
-        )}
       </div>
 
       <div className="Sitebar__btns Sitebar__logout">
