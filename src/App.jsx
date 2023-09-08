24ac18745d6b75fe74cd38518698b81379f7754a
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-08 09:37:10 +0500

    Guidelar uchun yangi Protected Route yozib oldim.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
 import { Protected } from "./components";
-import { Login, NotFound, Users } from "./pages";
+import { Guides, Login, NotFound, Users } from "./pages";
 import { Profile } from "./layouts";
 export const App = () => {
   const token = localStorage.getItem("token") ? true : false;
@@ -36,6 +36,15 @@ export const App = () => {
           element={
             <Protected isLoggedIn={token}>
               <Profile />
+            </Protected>
+          }
+        />
+
+        <Route
+          path="/guides"
+          element={
+            <Protected isLoggedIn={token}>
+              <Guides />
             </Protected>
           }
         />
