606ea145b41cee97846c5b79f1bc4db89e2bebc0
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-08 09:41:15 +0500

    keladigan data'ni malumotlarini mapping qilib yubordim.

--- src/layouts/guide-list/guide-list.jsx
+++ src/layouts/guide-list/guide-list.jsx
@@ -2,6 +2,12 @@ import "./guide-list.css";
 export const GuideList = ({ data }) => {
   return (
     <div className="GuideList">
+      {data.map((guide) => (
+        <div className="GuideList__row" key={guide.id}>
+          <h1>{guide.title}</h1>
+          <p>{guide.content}</p>
+        </div>
+      ))}
     </div>
   );
 };
