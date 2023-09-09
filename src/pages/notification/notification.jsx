963ec87c57c1fb5aece73b0abde7fb3d8f36458e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-09 19:21:25 +0500

    jsx va notifyni o'qidim deyish.

--- src/pages/notification/notification.jsx
+++ src/pages/notification/notification.jsx
@@ -1,4 +1,5 @@
 import "./notification.css";
+import { Sitebar } from "../../layouts";
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../../shared/services/axios";
 import { Loader, UserGuide } from "../../components";
@@ -6,9 +7,19 @@ import { HandleFetchError } from "../../shared/errors/clear-account";
 import { successNot } from "../../shared/toastfy";
 export const Notification = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  const handleReadNotify = (id) => {
+    axiosInstance.post(`/user-guides/${id}/read`).then(({ data: { data } }) => {
+      setLoading(true);
+      successNot("Siz Guideni O'qidingiz!");
+      fetchData();
+    });
+  };
 
   const fetchData = () => {
     axiosInstance
@@ -23,5 +34,25 @@ export const Notification = () => {
   };
   return (
     <div className="Notification__Dashboard">
+      <Sitebar />
+      <div className="Notification">
+        <div className="Notification__header">
+          <h1 className="Notification__header__title">Notifications</h1>
+        </div>
+        <div className="Notifications">
+          {loading ? (
+            <Loader />
+          ) : (
+            data.map((Userguides, idx) => (
+              <UserGuide
+                key={idx}
+                data={Userguides}
+                handleReadNotify={(id) => handleReadNotify(id)}
+              />
+            ))
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
