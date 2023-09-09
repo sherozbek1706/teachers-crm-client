949fb029b85e03f31cabf157887c88c213cd4fcc
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-09 19:20:43 +0500

    Malumotlarni fetch qildim va useEffect orqali yasb oldim.

--- src/pages/notification/notification.jsx
+++ src/pages/notification/notification.jsx
@@ -1,4 +1,26 @@
+import "./notification.css";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../../shared/services/axios";
+import { Loader, UserGuide } from "../../components";
+import { HandleFetchError } from "../../shared/errors/clear-account";
+import { successNot } from "../../shared/toastfy";
 export const Notification = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axiosInstance
+      .get("/user-guides")
+      .then(({ data: { data } }) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        HandleFetchError(err);
+      });
+  };
   return (
     <div className="Notification__Dashboard">
   );
