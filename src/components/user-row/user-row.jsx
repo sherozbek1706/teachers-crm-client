81409eba7e3beefd3bf9795f2a4d176f9c02c63e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-12 20:51:07 +0500

    Uerlarni yangi tablega ko'chirdim.

--- src/components/user-row/user-row.jsx
+++ src/components/user-row/user-row.jsx
@@ -20,26 +20,34 @@ export const UserRow = ({ data }) => {
 
   return (
     <Fragment>
-      {data.map((user) => (
-        <div className="Users__row" key={user.id}>
-          <p className="Users__text Users__firstname">{user.first_name}</p>
-          <p className="Users__text Users__lastname">{user.last_name}</p>
-          <p className="Users__text Users__role">{user.role}</p>
-          <p className="Users__text Users__age">{user.age}</p>
-          <p className="Users__text Users__username">@{user.username}</p>
-          <div className="Users__option">
-            <Link to={`/profile/${user.id}`}>
-              <AiFillEye className="Users__icons Users__view" />
-            </Link>
-            <Link to={`/edit/user/${user.id}`}>
-              <BiEdit className="Users__icons Users__edit" />
-            </Link>
-            <BiTrash
-              className="Users__icons Users__delete"
-              onClick={() => handleRemoveUser(user.id)}
-            />
-          </div>
-        </div>
+      {data.map((user, idx) => (
+        <tr key={idx}>
+          <td>{idx + 1}</td>
+
+          <td>{user.first_name}</td>
+
+          <td>{user.last_name}</td>
+
+          <td>{user.age}</td>
+
+          <td>{user.role}</td>
+
+          <td>@{user.username}</td>
+          <td>
+            <div className="Users__option">
+              <Link to={`/profile/${user.id}`}>
+                <AiFillEye className="Users__icons Users__view" />
+              </Link>
+              <Link to={`/edit/user/${user.id}`}>
+                <BiEdit className="Users__icons Users__edit" />
+              </Link>
+              <BiTrash
+                className="Users__icons Users__delete"
+                onClick={() => handleRemoveUser(user.id)}
+              />
+            </div>
+          </td>
+        </tr>
       ))}
     </Fragment>
   );
